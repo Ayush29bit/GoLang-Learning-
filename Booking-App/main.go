c8e8c90e269308d44bbc752fb42cab0b18497638
Ayush Tiwari <ayushzztiwari69@gmail.com>
2026-02-16 19:23:59 +0530

    fixed bugs

--- Booking-App/main.go
+++ Booking-App/main.go
@@ -58,8 +58,7 @@ func main() {
 		}
 		fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
-		noTicketsRemainign := remainingTickets == 0
-		if noTicketsRemainign {
+		if remainingTickets == 0 {
 			// end program
 			fmt.Println("Our conference is booked out. Come back next year.")
 			break
